576486b16df930a9882de233360cc527669b0088
sachinsarasambi <sachinsarasambi5151@gmail.com>
2022-10-07 00:34:13 +0530

    2Assignmentupated
    2Assignmentupated

---
+++ 01_Assignment-02.js
@@ -0,0 +1,46 @@
+var sweety="sweety";
+var cuite="cuite";
+console.log("Before swap");
+console.log("sweety = "+sweety);
+console.log("cuite = "+ cuite);
+var temp;
+temp=sweety;
+sweety=cuite;
+cuite=temp;
+console.log("-----------------------");
+console.log("After swap");
+console.log("sweety = "+sweety);
+console.log("cuite = "+ cuite);
+
+
+console.log("---------------------------------------------")
+
+var num1=100;
+var num2=200;
+var num3=300;
+console.log("Berfore swapping:",num1,num2,num3);
+var temp;
+ temp=num1;
+
+num1=num2;
+num2=num3;
+num3=temp;
+console.log("After swapping value:",num1,num2,num3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 01_Assignment_Js-01.js
@@ -0,0 +1,16 @@
+var my_FullName;
+console.log(my_FullName);
+my_FullName="Sachin sarasambi";
+console.log(my_FullName);
+my_FullName="sachin shivaji sarasambi";
+console.log(my_FullName);
+
+
+var my_love="i love only JS ";
+console.log(my_love)
+
+var FirstName="Sachin", lastName="sarasambi", age="20", Addtrress="pune", PinCode="411057", Gender="male",IsMarried="NO";
+console.log(FirstName,lastName,age,Addtrress,PinCode,Gender,IsMarried);
+
+
+
