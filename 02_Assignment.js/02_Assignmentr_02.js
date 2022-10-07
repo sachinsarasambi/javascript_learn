83a63b29a555a8be2be6f21449c1f508fb5854d2
sachinsarasambi <sachinsarasambi5151@gmail.com>
2022-10-07 14:37:09 +0530

    datatypeANDfuntionupdated
    datatypeANDfuntionupdated

---
+++ 02_Assignment.js/02_Assignmentr_02.js
@@ -0,0 +1,22 @@
+var Number=21;
+var name="sachin";
+var true1="true";
+var pin=4110565;
+
+console.log(typeof Number);
+console.log(typeof name);
+console.log(typeof true);
+console.log(typeof pin);
+console.log("====================NULL property===========================");
+var city=null;
+console.log(typeof city);
+console.log("=======================Undefined property==========================");
+
+var my_name;
+console.log( my_name)
+console.log("===============================");
+console.log(typeof my_name);
+
+
+
+
---
+++ 02_Assignment.js/02_assignment_1.js
@@ -0,0 +1,75 @@
+function sachin(){
+    console.log("write any two funmction")
+
+}
+sachin();
+
+function sandip(){
+    console.log("write function print console")
+}
+sandip();
+
+console.log("========================concat Funcation========================");
+
+function concat(firstName,LastName){
+    console.log(firstName+LastName);
+        }
+concat("kapil","sharama");
+
+console.log("=================================================================================================");
+
+function swap_values(arg1,arg2){
+    console.log("======================Before swapping==================");
+    console.log(arg1,arg2)
+    console.log("=================After  swapping====================");
+    var temp=arg1
+    arg1=arg2
+    arg2=temp
+    console.log(arg1,arg2);
+
+}
+swap_values("virat","Anushka");
+swap_values(1000,2000)
+console.log("=================================================================================================");
+
+console.log("=======================Three adition Number==============================");
+
+function add_three_numbers(num1,num2,num3){
+
+    // console.log("========Addition pro============================");
+    console.log(num1+num2+num3);
+
+}
+add_three_numbers(10,23,600);
+add_three_numbers("Hello","Good","Morning")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
