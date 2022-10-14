8624d89cefb4e7c78b4411f9503073e82feacfc6
sachinsarasambi <sachinsarasambi5151@gmail.com>
2022-10-14 19:43:22 +0530

    updated controlflow new edit
    updated controlflow new edit

--- 05_Assignment_01.js
+++ 05_Assignment_01.js
@@ -1,31 +1,41 @@
-console.log("=======================================================");
-var wordLengthSqure=function (word) {
-    var strinlength=word.length;
-    return strinlength*strinlength;
+// console.log("=======================================================");
+// var wordLengthSqure=function (word) {
+//     var strinlength=word.length;
+//     return strinlength*strinlength;
 
 
-}
-console.log("====================Square of string length============================");
-console.log("Square of string length: "+wordLengthSqure("JavaScript"));
-console.log("Square of string length:"+ wordLengthSqure("Google"));
-console.log("Square of string length: "+wordLengthSqure("Devloper"));
+// }
+// console.log("====================Square of string length============================");
+// console.log("Square of string length: "+wordLengthSqure("JavaScript"));
+// console.log("Square of string length:"+ wordLengthSqure("Google"));
+// console.log("Square of string length: "+wordLengthSqure("Devloper"));
 
-console.log("====================================================");
-function name(params) {
-    var given_string="i am Angular Developer".length;
+// console.log("====================================================");
+// function name(params) {
+//     var given_string="i am Angular Developer".length;
 
-     var res="i am Angular Developer".split(" ");
-     var Total_Length=res.length;
-     var Division=given_string/Total_Length;
-     var Multi=given_string*Total_Length;
-     console.log("Multification by Given_stringand Total_length: "+Division);
-     console.log("Multification by Given_stringand Total_length: "+Multi);
+//      var res="i am Angular Developer".split(" ");
+//      var Total_Length=res.length;
+//      var Division=given_string/Total_Length;
+//      var Multi=given_string*Total_Length;
+//      console.log("Multification by Given_stringand Total_length: "+Division);
+//      console.log("Multification by Given_stringand Total_length: "+Multi);
 
     
-}
-name();
+// }
+// name();
 
+var playerOne=120;
+var playertwo=130;
+var playerthree=140;
+var playerfour=150;
+var playerfive=160;
 
+console.log("console.log"+" "+playerOne);
+console.debug("console.debug"+" "+playertwo);
+console.warn("console.warn"+" "+playerthree);
+console.info("console.info"+" "+playerfour);
+console.error("console.error"+" "+playerfive);
 
 
     
---
+++ 07_Assignment_switch02.js
@@ -0,0 +1,58 @@
+var year=function(month){
+
+
+switch(month){
+    case 1:
+        console.log("january");
+        break;
+
+        case 2:
+        console.log("February");
+        break;
+
+        case 3:
+        console.log("mMarch");
+        break;
+
+        case 4:
+        console.log("April");
+        break;
+
+        case 5:
+        console.log("May");
+        break;
+
+        case 6:
+        console.log("June");
+        break;
+
+        case 7:
+        console.log("Jully");
+        break;
+
+        case 8:
+        console.log("Aygust");
+        break;
+
+        case 9:
+        console.log("September");
+        break;
+        case 10:
+        console.log("Octomber");
+        break;
+        case 11:
+        console.log("November");
+        break;
+        case 12:
+            console.log("Descmber");
+            break;
+            default:
+                console.log("Invali input");
+                break;
+}
+
+}
+year(1);
+year(2);
+year(4);
+year(10);
